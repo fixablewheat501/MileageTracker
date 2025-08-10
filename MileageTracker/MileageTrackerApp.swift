9cd7d12f73fc2f42e753931ac35ac21823026ed4
fixablewheat501 <63933390+fixablewheat501@users.noreply.github.com>
2025-08-10 19:18:56 -0400

    Initial Commit

---
+++ MileageTracker/MileageTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MileageTrackerApp.swift
+//  MileageTracker
+//
+//  Created by Andrew Black on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MileageTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MileageTrackerTests/MileageTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MileageTrackerTests.swift
+//  MileageTrackerTests
+//
+//  Created by Andrew Black on 8/10/25.
+//
+
+import Testing
+@testable import MileageTracker
+
+struct MileageTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
